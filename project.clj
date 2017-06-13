59bde8fc7f74bc4e6b3a9d97adb5c5df1b0c5aea
Peter McCulloch <peter.mcculloch@gmail.com>
2017-06-13 17:54:12 -0400

    Core code

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject olivier "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/data.generators "0.1.2"]
+                 [org.clojure/math.combinatorics "0.1.1"]
+                 ]
+  :main ^:skip-aot olivier.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
